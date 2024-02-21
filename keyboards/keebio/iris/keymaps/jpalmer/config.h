a1aa96b14a20d12ff8bf6a89387d8fd2a606abb8
Jeff Palmer <jeffrey.palmer@acm.org>
2024-02-20 16:51:49 -0800

    Sync with upstream
    Upgrade my layout to work with current QMK version

--- keyboards/keebio/iris/keymaps/jpalmer/config.h
+++ keyboards/keebio/iris/keymaps/jpalmer/config.h
@@ -22,8 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PERMISSIVE_HOLD
 
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // #define USE_I2C
 #define EE_HANDS
 
--- keyboards/keebio/iris/keymaps/jpalmer/keymap.c
+++ keyboards/keebio/iris/keymaps/jpalmer/keymap.c
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     RESET,   _______, _______, _______, _______, KX_CLMK,                            KX_MDDH, _______, _______, _______, _______, _______,
+     QK_BOOT, _______, _______, _______, _______, KX_CLMK,                            KX_MDDH, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, _______,                            KC_VOLU, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -196,21 +196,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
   return true;
-}
-
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
 }
